5f9651113f7b2f2bde171d6db0df03c8d2026f64
Franck Kirmann <franck.kirmann@gmail.com>
2024-02-02 15:24:06 +0100

    version 1.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,5 @@
-const Quagga = require("quagga");
+const Add = document.getElementById("Add");
 
-// Configurez les paramètres du lecteur
-const config = {
-  inputStream: {
-    name: "Live",
-    type: "LiveStream",
-    target: document.querySelector("#barcode-scanner"), // Div où le flux vidéo sera affiché
-    constraints: {
-      width: 480,
-      height: 320,
-      facingMode: "environment", // Utilisez la caméra arrière (si disponible)
-    },
-  },
-  decoder: {
-    readers: ["code_128_reader", "ean_reader"], // Types de codes-barres pris en charge
-  },
-};
-
-// Initialisez le lecteur avec les paramètres configurés
-Quagga.init(config, function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  // Démarrez le lecteur
-  Quagga.start();
+Add.addEventListener("click", () => {
+  window.location.href = "test.html";
 });
-
-// Gérez l'événement de détection d'un code-barres
-Quagga.onDetected(function (result) {
-  // Accédez aux données du code-barres détecté
-  const code = result.codeResult.code;
-  console.log("Code-barres détecté : " + code);
-
-  // Arrêtez le lecteur après avoir trouvé un code-barres (à ajuster selon vos besoins)
-  Quagga.stop();
-});
-
-// Reste du code Quagga...
