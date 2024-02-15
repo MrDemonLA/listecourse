2fc8b7b660f36005d3816c3170873dde7a15d6e4
Franck Kirmann <franck.kirmann@gmail.com>
2024-02-15 16:09:33 +0100

    version 2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,5 +4,3 @@ const ClearList = document.querySelector(".ClearList");
 const confirmationModal = document.getElementById("confirmationModal");
 const cancelButton = document.getElementById("cancelButton");
 const confirmButton = document.getElementById("confirmButton");
-
-const TabList = ["coca", "céréale", "mouchoir", "savon mains"];
