b10332cd7ea831ba81379bdcd551205be26b8e91
Franck Kirmann <franck.kirmann@gmail.com>
2024-02-20 02:18:58 +0100

    version 1.5

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,6 @@ fetch("https://api-list-iy8c.vercel.app/afficher")
   .then((response) => response.json())
   .then((data) => {
     const TabList = data.TabList;
-    refreshList(TabList);
     console.log(TabList); // Accéder aux données JSON
     console.log("Tableau récupéré depuis l'API :", data.TabList);
   })
